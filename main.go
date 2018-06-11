69c3f1e59d5cd1f16280e239acc9f8d1c332d628
malumolo <38481375+malumolo@users.noreply.github.com>
2018-06-10 17:11:05 -0700

    Update main.go

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.48.141:9200"
+	ES_URL = "http://104.196.179.197:9200"
 )
 
 func main() {
