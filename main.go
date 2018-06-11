db43afffb2aa292b9dc28074ca2a96e7b142892e
malumolo <38481375+malumolo@users.noreply.github.com>
2018-06-10 17:18:41 -0700

    Update main.go

--- main.go
+++ main.go
@@ -175,5 +175,6 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
+	
 
 }
